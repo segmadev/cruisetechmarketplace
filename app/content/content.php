7cb14cb3f2bf243cd8c65756d974a9bc99a2c2e3
net <71606399+serikioluwagbenga@users.noreply.github.com>
2024-04-29 21:02:38 +0100

    remove t&c link

--- app/content/content.php
+++ app/content/content.php
@@ -363,7 +363,7 @@ class content extends database
     }
     function terms_message()
     {
-        return "<b>By proceeding you agree to our <a href='https://proloomtrading.com/page.php?t=terms_and_conditions' target='_blank'>team and conditions</a> and <a target='_blank' href='https://proloomtrading.com/page.php?t=policy'> privacy policy</a>.</b>";
+        return "<b>By proceeding you agree to our <a href='#' target='_blank'>team and conditions</a> and <a target='_blank' href='#'> privacy policy</a>.</b>";
     }
 
     function arrow_percentage($percent, $word = "profit")
