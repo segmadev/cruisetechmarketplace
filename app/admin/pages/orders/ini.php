14307992d2618b6962a575b4e3dd6b170c4e1cb6
net <71606399+serikioluwagbenga@users.noreply.github.com>
2024-05-12 16:00:57 +0100

    make all orders in descending order by date

--- app/admin/pages/orders/ini.php
+++ app/admin/pages/orders/ini.php
@@ -1,17 +1,17 @@
 <?php
 if(isset($_GET['userID'])) {
-    $orders = $d->getall("orders", "userID = ?", [htmlspecialchars($_GET['userID'])], fetch:"all" );
+    $orders = $d->getall("orders", "userID = ? order by date DESC", [htmlspecialchars($_GET['userID'])], fetch:"all" );
 }
 
 if(isset($_GET['id'])) {
     $id = htmlspecialchars($_GET['id']);
     $order = $d->getall("orders", "ID = ?", [$id]);
-    $account = $d->getall("account", "ID = ?", [$order['accountID']]);
+    $account = $d->getall("account", "ID = ? order by date DESC", [$order['accountID']]);
     $logins = explode(',', $order['loginIDs']);
 }
 
 if(!isset($orders) && !isset($order)) {
-    $orders = $d->getall("orders", fetch:"all");
+    $orders = $d->getall("orders", "ID != ? order by date DESC", [""], fetch:"all");
 }
 
 require_once "functions/account.php";
--- app/pages/orders/ini.php
+++ app/pages/orders/ini.php
@@ -2,12 +2,12 @@
     require_once "functions/account.php";
     $a = new Account;
     if(!isset($_GET['id'])) {   
-        $orders = $d->getall("orders", "userID = ?", [$userID], fetch:"moredetails");
+        $orders = $d->getall("orders", "userID = ? order by date DESC", [$userID], fetch:"moredetails");
     }
 
     if(isset($_GET['id'])) {
         $id = htmlspecialchars($_GET['id']);
-        $order = $d->getall("orders", "ID = ? and userID = ?", [$id, $userID]);
+        $order = $d->getall("orders", "ID = ? and userID = ? order by date DESC", [$id, $userID]);
         $account = $d->getall("account", "ID = ?", [$order['accountID']]);
         $logins = explode(',', $order['loginIDs']);
     }
