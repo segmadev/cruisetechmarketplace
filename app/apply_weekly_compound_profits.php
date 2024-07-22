2a9feadbd1a60ce32d9e73a51a46975b96feb21f
net <71606399+serikioluwagbenga@users.noreply.github.com>
2024-07-22 18:07:03 +0100

    removed all unused files

--- app/apply_weekly_compound_profits.php
+++
@@ -1,4 +0,0 @@
-<?php 
-    require_once "include/cron-ini.php";
-    $i->apply_weekly_compound_profits();
-?>
--- app/take_pending_trades.php
+++
@@ -1,4 +0,0 @@
-<?php 
-require_once "include/cron-ini.php";
-$no = htmlspecialchars($_GET['no'] ?? 25);
-$i->take_pending_trades($no, htmlspecialchars($_GET['type'] ?? "user"), htmlspecialchars($_GET['order'] ?? "ASC"));
