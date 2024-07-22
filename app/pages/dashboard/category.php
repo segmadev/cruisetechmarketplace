43e5bfe58863206f6f0fdf6f6376cce2e8381b31
net <71606399+serikioluwagbenga@users.noreply.github.com>
2024-07-22 19:10:39 +0100

    change text color for category name

--- app/pages/dashboard/category.php
+++ app/pages/dashboard/category.php
@@ -3,7 +3,7 @@
 ?>
 <div class="card">
     <div class="card-header d-flex justify-content-between">
-        <div class="title bg-primary p-2 fs-3"><b><?= $category['name'] ?></b></div>
+        <div class="title bg-primary text-white p-2 fs-3"><b><?= $category['name'] ?></b></div>
         <div class="buttons"><a href="index?action=view&category=<?= $category['ID'] ?>" class="btn btn-sm btn-primary">View All</a></div>
     </div>
     <div class="card-body w-100 row row-cols-1 row-cols-lg-3 row-cols-md-2 g-1 g-lg-3 m-0 p-0">
