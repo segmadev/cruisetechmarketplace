8fa26e1e32e6b7103ff04d1635424c99c358a3aa
net <71606399+serikioluwagbenga@users.noreply.github.com>
2024-08-04 13:38:32 +0100

    correct errors in webhook for testing

--- app/validate_webhook.php
+++ app/validate_webhook.php
@@ -1,4 +1,6 @@
 <?php 
+header("Content-Type: application/json; charset=UTF-8");
+require_once "consts/main.php";
 require_once "admin/include/database.php";
 class validate_payment extends database {
 
@@ -7,7 +9,7 @@ class validate_payment extends database {
         $jsonData = file_get_contents('php://input');
         // Decode the JSON data into a PHP associative array
         $data = json_decode($jsonData, true);
-        $data = (array)$data['data'];
+        $data = (array)$data;
         $data2 = $_POST;
         // Check if decoding was successful
         try {
@@ -22,37 +24,19 @@ class validate_payment extends database {
    
     function storeResult($data) {
         $filename = "results.db";
-        $tempFilename = "temp_datainfo.db";
         $newData = "\n" . json_encode($data); 
         // Open the temporary file for writing
-        $tempFile = fopen($tempFilename, "w") or die("Unable to open temporary file!");
+        $tempFile = fopen($filename, "w") or die("Unable to open temporary file!");
         // Write the new data to the temporary file
         fwrite($tempFile, $newData);
         // Check if the original file exists
-        if (!file_exists($filename)) { 
-            $originalFile = fopen($filename, "w");
-            fclose($originalFile);
-        }
-        // Open the original file for reading
-        $originalFile = fopen($filename, "r") or die("Unable to open original file!");
-        // Append the content of the original file to the temporary file
-        while (!feof($originalFile)) {
-            $line = fgets($originalFile);
-            if ($line !== false) {
-                fwrite($tempFile, $line);
-            }
-        }   
-        // Close the original file
-        fclose($originalFile);
-        // Close the temporary file
         fclose($tempFile);
-        // Replace the original file with the temporary file
-        rename($tempFilename, $filename) or die("Unable to rename temporary file!");        
+        return true;
     }
     function apiMessage($message, int $code = 400, $data = null) {
         header(':', true, $code);
         return json_encode(["code" => $code, "message" => $message, "data" => $data], true);
     }
 }
-    
-?>
+$vaildate = new validate_payment();
+echo $vaildate->getAndStoreResult();
