a1fc3fbc8eefcd4fc1aa9625dc613abd01650611
net <71606399+serikioluwagbenga@users.noreply.github.com>
2024-08-25 23:43:21 +0100

    Richtext for account description

--- app/admin/consts/account.php
+++ app/admin/consts/account.php
@@ -4,7 +4,7 @@
         "ID"=>["input_type"=>"hidden", "is_required"=>false],
         "platformID"=>["title"=>"Select Platfrom","type"=>"select", "options"=>$d->options_list("platform"), "description"=>"<a href='index?p=platform&action=new'>Click here</a> to Create New Platfrom"],
         "categoryID"=>["title"=>"Select Category","type"=>"select", "options"=>$d->options_list("category"), "description"=>"<a href='index?p=category&action=new'>Click here</a> to Create New Category"],
-        "title"=>["description"=>"Name or title of the account"],
+        "title"=>["description"=>"Name or title of the account", "id"=>"richtext2", "global_class"=>"col-md-12",],
         "description"=>["type"=>"textarea", "title"=>"Description (optional)", "is_required"=>false, "description"=>"Tell buyers more about the account"],
         "amount"=>["input_type"=>"number", "description"=>"What is the amount you selling the account for"],
         "Aditional_auth_info"=>["id"=>"richtext", "global_class"=>"col-md-12", "title"=>"Aditional Authentication Information (optional)", "is_required"=>false, "type"=>"textarea", "description"=>"Enter additional details about the account,<span class='text-danger'>This won't show until the user purchases the account.</span>"],
