39726086c9c3df67dcd6a5935b19d60d704fa45f
net <71606399+serikioluwagbenga@users.noreply.github.com>
2024-09-17 11:01:06 +0100

    Trans table adjustment

--- app/admin/pages/users/trans_table.php
+++ app/admin/pages/users/trans_table.php
@@ -9,11 +9,12 @@ $script[] = "table";
             <tr>
                 <th>ID</th>
                 <th>user</th>
-                <th>Reason</th>
-                <th>Action</th>
-                <th>Account Type</th>
                 <th>Amount</th>
                 <th>Current Balance</th>
+                <th>ForID</th>
+                <th>Reason</th>
+                <th>Action</th>
+                <!-- <th>Account Type</th> -->
                 <th>Date</th>
             </tr>
             <!-- end row -->
@@ -27,12 +28,12 @@ $script[] = "table";
                 <tr>
                     <td><?= $row['ID'] ?></td>
                     <td class="d-flex gap-2"><?= $u->profile_picture_default($row['userID']) . ucfirst($full_name); ?></td>
-                   
-                    <td><?= $d->short_text($row['trans_for']) ?></td>
-                    <td class='text-<?= $row['action_type'] == 'debit' ? 'danger' : 'success'; ?>'><?= str_replace("_", " ", $row['action_type']) ?></td>
-                    <td><?= str_replace("_", " ", $row['acct_type']) ?></td>
                     <td class='text-<?= $row['action_type'] == 'debit' ? 'danger' : 'success'; ?>'><?= $d->money_format($row['amount'], currency) ?></td>
                     <td><?= $d->money_format($row['current_balance'], currency) ?></td>
+                    <td><?= $d->short_text($row['forID']) ?></td>
+                    <td><?= $d->short_text($row['trans_for']) ?></td>
+                    <td class='text-<?= $row['action_type'] == 'debit' ? 'danger' : 'success'; ?>'><?= str_replace("_", " ", $row['action_type']) ?></td>
+                    <!-- <td><?= str_replace("_", " ", $row['acct_type']) ?></td> -->
                     <td><?= $d->date_format($row['date']) ?></td>
                 </tr>
                 <!-- end row -->
