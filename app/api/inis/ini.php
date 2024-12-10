f0ae2d39fe84aeabfbe1b078f2ebd853dbf4b90d
net <71606399+serikioluwagbenga@users.noreply.github.com>
2024-12-10 13:26:00 +0100

    api debug

--- app/api/inis/ini.php
+++ app/api/inis/ini.php
@@ -1,26 +1,30 @@
 <?php
-    header("Content-Type: application/json; charset=UTF-8");
-    // Remove the "/cruise/" prefix from the URI
-    $iniPath = str_replace("/cruise/", "", $_SERVER['REQUEST_URI']);
+header("Content-Type: application/json; charset=UTF-8");
+// Remove the "/cruise/" prefix from the URI
+$iniPath = str_replace("/cruise/", "", $_SERVER['REQUEST_URI']);
 
-    // Parse the URL to extract only the path
-    $parsedUrl = parse_url($iniPath);
-    $cleanPath = rtrim($parsedUrl['path'], '/'); // Remove trailing slash if it exists
+// Parse the URL to extract only the path
+$parsedUrl = parse_url($iniPath);
+$cleanPath = rtrim($parsedUrl['path'], '/'); // Remove trailing slash if it exists
 
-    // Define PATH without GET parameters
-    define("PATH", str_replace("app/api", "", $cleanPath));
-    // define("PATH", $cleanPath);
+// Define PATH without GET parameters
+define("PATH", str_replace("app/api", "", $cleanPath));
+// define("PATH", $cleanPath);
 
-    // Parse the query string into $_GET if present
-    if (isset($parsedUrl['query'])) {
-        parse_str($parsedUrl['query'], $_GET);
-    }
-    require_once "../consts/main.php";
-    require_once "../admin/include/database.php";
-    $d = new database;
-    $token = $d->getBearerToken();
-    // load env data
-    require_once '../vendor/autoload.php';
-    use Dotenv\Dotenv;
-    $dotenv = Dotenv::createImmutable(rootFile);
-    $dotenv->load();
+var_dump(PATH);
+exit();
+// Parse the query string into $_GET if present
+if (isset($parsedUrl['query'])) {
+    parse_str($parsedUrl['query'], $_GET);
+}
+require_once "../consts/main.php";
+require_once "../admin/include/database.php";
+$d = new database;
+$token = $d->getBearerToken();
+// load env data
+require_once '../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(rootFile);
+$dotenv->load();
