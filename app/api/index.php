d7b7e8ddb142b37fbcc17f32cc38b8b629d5ceb2
net <71606399+serikioluwagbenga@users.noreply.github.com>
2024-12-11 18:06:57 +0100

    API debug: api index retore

--- app/api/index.php
+++ app/api/index.php
@@ -1,12 +1,8 @@
 <?php
 // phpinfo();
 // exit();
-header("Content-Type: application/json; charset=UTF-8");
-// Remove the "/cruise/" prefix from the URI
-$iniPath = str_replace("/cruise/", "", $_SERVER['REQUEST_URI']);
-var_dump($iniPath);
-// ini_set('display_errors', 1);
-// ini_set('display_startup_errors', 1);
-// error_reporting(E_ALL);
-//     require_once "inis/ini.php";
-//     require_once "router.php";
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+require_once "inis/ini.php";
+require_once "router.php";
